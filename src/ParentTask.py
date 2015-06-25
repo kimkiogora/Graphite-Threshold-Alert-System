6432848b5d7165931b4445fde5bcc50ce151fade
Kim Kiogora <kimkiogora@gmail.com>
2015-06-25 16:56:31 +0300

    Create ParentTask.py

---
+++ src/ParentTask.py
@@ -0,0 +1,22 @@
+################################################################################################
+# @Author	: Kim Kiogora <kimkiogora@gmail.com>
+# @version	: 2.0 , 15/01/12
+# @Usage
+##################################################################################################
+class Task:
+    # variables
+    global mydata;
+
+    # constructor
+    def __init__(self, data):
+        """Pass your data here"""
+        self.mydata = data
+
+    # Process data & alert
+    def processData(self):
+        """Do something with your data, mydata variable """
+
+
+#############################################
+# END THE  JOB CLASS
+#############################################
