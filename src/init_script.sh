7dd21d413ad701acd5f2e3309432132666f8f4d3
Kim Kiogora <kimkiogora@gmail.com>
2015-06-26 12:04:07 +0300

    Update init_script.sh

--- src/init_script.sh
+++ src/init_script.sh
@@ -1,63 +1,64 @@
-#!/bin/sh
-### This file is borrowed from a different source ####
-### <kimkiogora@gmail.com>
+#!/bin/bash
+# Author kim kiogora <kimkiogora@gmail.com>
+APPNAME=ThresholdAlerter
+script_home=/home/kim/PycharmProjects/GTA
+script_name="$script_home/$APPNAME.py"
+pid_file="/var/run/$APPNAME.pid"
 
-### BEGIN INIT INFO
-# Provides:          myservice
-# Required-Start:    $remote_fs $syslog
-# Required-Stop:     $remote_fs $syslog
-# Default-Start:     2 3 4 5
-# Default-Stop:      0 1 6
-# Short-Description: Put a short description of the service here
-# Description:       Put a long description of the service here
-### END INIT INFO
- 
-# Change the next 3 lines to suit where you install your script and what you want to call it
-DIR=src/
-DAEMON=$DIR/ThresholdAlerter.py
-DAEMON_NAME=GraphiteThresholdAlerter
- 
-# Add any command line options for your daemon here
-DAEMON_OPTS=""
- 
-# This next line determines what user the script runs as.
-# Root generally not recommended but necessary if you are using the Raspberry Pi GPIO from Python.
-DAEMON_USER=root
- 
-# The process ID of the script when it runs is stored here:
-PIDFILE=/var/run/$DAEMON_NAME.pid
- 
-. /lib/lsb/init-functions
- 
-do_start () {
-    log_daemon_msg "Starting system $DAEMON_NAME daemon"
-    start-stop-daemon --start --background --pidfile $PIDFILE --make-pidfile --user $DAEMON_USER --chuid $DAEMON_USER --startas $DAEMON -- $DAEMON_OPTS
-    log_end_msg $?
+# returns a boolean and optionally the pid
+running() {
+    local status=false
+    if [[ -f $pid_file ]]; then
+        # check to see it corresponds to the running script
+        local pid=$(< "$pid_file")
+        local cmdline=/proc/$pid/cmdline
+        # you may need to adjust the regexp in the grep command
+        if [[ -f $cmdline ]] && grep -q "$script_name" $cmdline; then
+            status="true $pid"
+        fi
+    fi
+    echo $status
 }
-do_stop () {
-    log_daemon_msg "Stopping system $DAEMON_NAME daemon"
-    start-stop-daemon --stop --pidfile $PIDFILE --retry 10
-    log_end_msg $?
+
+start() {
+    echo "starting $script_name"
+    nohup python "$script_name" &
+    echo $! > "$pid_file"
 }
- 
-case "$1" in
- 
-    start|stop)
-        do_${1}
+
+stop() {
+    # `kill -0 pid` returns successfully if the pid is running, but does not
+    # actually kill it.
+    kill -0 $1 && kill $1
+    rm "$pid_file"
+    echo "stopped"
+}
+
+read running pid < <(running)
+
+case $1 in 
+    start)
+        if $running; then
+            echo "$script_name is already running with PID $pid"
+        else
+            start
+        fi
         ;;
- 
-    restart|reload|force-reload)
-        do_stop
-        do_start
+    stop)
+        stop $pid
         ;;
- 
+    restart)
+        stop $pid
+        start
+        ;;
     status)
-        status_of_proc "$DAEMON_NAME" "$DAEMON" && exit 0 || exit $?
+        if $running; then
+            echo "$script_name is running with PID $pid"
+        else
+            echo "$script_name is not running"
+        fi
         ;;
-    *)
-        echo "Usage: /etc/init.d/$DAEMON_NAME {start|stop|restart|status}"
-        exit 1
+    *)  echo "usage: $0 <start|stop|restart|status>"
+        exit
         ;;
- 
 esac
-exit 0
