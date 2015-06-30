e547252858ae74ece39c45363df8dd2e280f3587
Kim Kiogora <kimkiogora@gmail.com>
2015-06-30 15:25:59 +0300

    Update ThresholdAlerter.py

--- src/ThresholdAlerter.py
+++ src/ThresholdAlerter.py
@@ -1,7 +1,7 @@
-# @Author   : Kim Kiogora	<kimkkiogora@gmail.com>
+# @Author	: Kim Kiogora	<kimkkiogora@gmail.com>
 # @Usage    : Alerts for graphite
-# Version   : 1.0
-# Since     : 28 June 2015
+# Version   1.0
+# Since     28 June 2015
 import time
 from threading import Thread
 from QueuePool import QueueManager
@@ -15,7 +15,6 @@ CUtils = CUtility()
 
 # The class itself
 class Parent:
-    global my_pool
 
     # An empty constructor
     def __init__(self):
@@ -26,10 +25,6 @@ class Parent:
         else:
             CUtils.infoLog.error('Config file = ' + CUtils.CONFIG_FILE + ' not found, exiting...')
             self.die('Property file does not exist!')
-
-        CUtils.logger("Initializing the queue with workers")
-        """ Initialize the pool with  workers """
-        self.my_pool = QueueManager(CUtils.MAX_CHILD_PROCESSES)
 
     # Wait a few seconds before proceeding with a task
     def do_wait(self, sec):
@@ -54,6 +49,9 @@ class Parent:
         CUtils.logger('starting up on %s port %s' % server_address)
         sock.bind(server_address)
         sock.listen(CUtils.MAX_CHILD_PROCESSES)
+        CUtils.logger("Initializing the queue with workers")
+        """ Initialize the pool with  workers """
+        my_pool = QueueManager(CUtils.MAX_CHILD_PROCESSES)
         while True:
             # Wait for a connection
             CUtils.logger('waiting for a connection')
@@ -69,12 +67,12 @@ class Parent:
                 connection.close()
                 CUtils.logger('Closed connection, now working on data...')
                 # Close immediately to prevent Socket Blocking
-                # Improved processing
-                self.my_pool.add_task(self.create_task,data)
-                self.my_pool.wait_completion()
+                my_pool.add_task(self.create_task, data)
+                my_pool.wait_completion()
             except Exception, ex:
                 CUtils.logger("Exception occurred %s, continue waiting for new connection" % str(ex))
-                connection.close()
+                if connection is not None:
+                    connection.close()
 
     # function to start the service process
     def start(self):
